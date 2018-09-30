bb265f45a4caa1db77aabc30dc2888294498f686
Carlos henrique <henricarlos.n@gmail.com>
2018-09-30 19:47:19 -0300

    Commit inicial do projeto

---
+++ api/Invetario_SF/Api_Inventario_SF/Api_Inventario_SF.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Microsoft.ServiceFabric.Data;
+
+namespace Api_Inventario_SF
+{
+    /// <summary>
+    /// O FabricRuntime cria uma instância dessa classe para cada instância de tipo de serviço. 
+    /// </summary>
+    internal sealed class Api_Inventario_SF : StatelessService
+    {
+        public Api_Inventario_SF(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Substituição opcional para criar ouvintes (como tcp e http) para essa instância do serviço.
+        /// </summary>
+        /// <returns>A coleção de ouvintes.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener(serviceContext =>
+                    new KestrelCommunicationListener(serviceContext, "ServiceEndpoint", (url, listener) =>
+                    {
+                        ServiceEventSource.Current.ServiceMessage(serviceContext, $"Starting Kestrel on {url}");
+
+                        return new WebHostBuilder()
+                                    .UseKestrel()
+                                    .ConfigureServices(
+                                        services => services
+                                            .AddSingleton<StatelessServiceContext>(serviceContext))
+                                    .UseContentRoot(Directory.GetCurrentDirectory())
+                                    .UseStartup<Startup>()
+                                    .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
+                                    .UseUrls(url)
+                                    .Build();
+                    }))
+            };
+        }
+    }
+}
---
+++ api/Invetario_SF/Api_Inventario_SF/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.ServiceFabric.Services.Runtime;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Api_Inventario_SF
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Este é o ponto de entrada do processo de host de serviço.
+        /// </summary>
+        private static void Main()
+        {
+            try
+            {
+                // O arquivo ServiceManifest.XML define um ou mais nomes de tipo de serviço.
+                // Registrar um serviço mapeia um nome de tipo de serviço para um tipo de .NET.
+                // Quando o Service Fabric cria uma instância deste tipo de serviço,
+                // uma instância da classe é criada neste processo de host.
+
+                ServiceRuntime.RegisterServiceAsync("Api_Inventario_SFType",
+                    context => new Api_Inventario_SF(context)).GetAwaiter().GetResult();
+
+                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(Api_Inventario_SF).Name);
+
+                // Impede que este processo de host seja encerrado para que os serviços continuem em execução. 
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception e)
+            {
+                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+                throw;
+            }
+        }
+    }
+}
